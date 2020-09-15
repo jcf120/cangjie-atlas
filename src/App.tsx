8567f29962a1009a5605f74f572a9a61a42c3f8c
Joss Clifford-Frith <jossie.cliffordfrith@gmail.com>
2020-09-15 18:56:08 +0100

    limit input keys

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,31 @@ import React, { useEffect, useRef, useState } from "react";
 import style from "./App.module.css";
 import Layout from "./Layout";
 
+const acceptedKeys = "qwertyuiopasdfghjklzxcvbnm";
+
+function useScopeInput() {
+  const [scope, setScope] = useState("");
+  useEffect(() => {
+    document.addEventListener("keydown", (e) => {
+      if (acceptedKeys.includes(e.key)) {
+        setScope((scope) => scope + e.key);
+      } else if (e.code === "Backspace") {
+        setScope((scope) => scope.substring(0, scope.length - 1));
+      }
+    });
+  });
+  return scope;
+}
+
 function App() {
   const ref = useRef<HTMLDivElement>(null);
   const [width, setWidth] = useState<number>(0);
-  const [scope, setScope] = useState("");
   useEffect(() => {
     if (ref.current) {
       setWidth(ref.current.offsetWidth);
     }
-    document.addEventListener("keydown", (e) => {
-      setScope((scope) =>
-        e.code === "Backspace"
-          ? scope.substring(0, scope.length - 1)
-          : scope + e.key
-      );
-    });
   }, [ref]);
+  const scope = useScopeInput();
   return (
     <div className={style.root} ref={ref}>
       {!!width && <Layout hideChars width={width} scope={scope} />}
