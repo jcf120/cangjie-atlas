ef60eb8291ec87e998914efbdbc22c945c783191
Joss Clifford-Frith <jossie.cliffordfrith@gmail.com>
2020-09-23 20:07:07 +0100

    Readabilty tweaks and add key shadows

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -36,7 +36,7 @@ function drawRecursive(
   const layout = layoutsData[scope];
   if (!layout) return;
   const unit = width / units;
-  ctx.font = `${unit * ((units - 4) / units)}px Helvetica`;
+  ctx.font = `300 ${unit * ((units - 3) / units)}px sans-serif`;
   for (let i = 0; i < row1Keys.length; i++) {
     const key = row1Keys[i];
     const chars = layout[key];
@@ -61,14 +61,14 @@ function drawRecursive(
   if (unit > 4) {
     for (let i = 0; i < row1Keys.length; i++) {
       const key = row1Keys[i];
-      drawRecursive(ctx, x + unit * i, y + unit, unit, scope + key);
+      drawRecursive(ctx, x + unit * i, y + unit * 0.9, unit, scope + key);
     }
     for (let i = 0; i < row2Keys.length; i++) {
       const key = row2Keys[i];
       drawRecursive(
         ctx,
         x + unit * 0.2 + unit * i,
-        y + unit * 2,
+        y + unit * 1.9,
         unit,
         scope + key
       );
@@ -78,11 +78,40 @@ function drawRecursive(
       drawRecursive(
         ctx,
         x + unit * 0.4 + unit * i,
-        y + unit * 3,
+        y + unit * 2.9,
         unit,
         scope + key
       );
     }
+  }
+}
+
+function drawQwerty(ctx: CanvasRenderingContext2D, width: number) {
+  const unit = width / units;
+  ctx.font = `bold ${unit}px sans-serif`;
+  for (let i = 0; i < row1Keys.length; i++) {
+    ctx.fillText(
+      row1Keys[i].toUpperCase(),
+      unit * 0.5 + unit * i,
+      unit * 1.2,
+      unit
+    );
+  }
+  for (let i = 0; i < row2Keys.length; i++) {
+    ctx.fillText(
+      row2Keys[i].toUpperCase(),
+      unit * 0.7 + unit * i,
+      unit * 2.2,
+      unit
+    );
+  }
+  for (let i = 0; i < row3Keys.length; i++) {
+    ctx.fillText(
+      row3Keys[i].toUpperCase(),
+      unit * 0.9 + unit * i,
+      unit * 3.2,
+      unit
+    );
   }
 }
 
@@ -96,6 +125,9 @@ export default function Layout({ scope }: { scope: string }) {
     if (ctx && canvas) {
       const rect = canvas.getBoundingClientRect();
       ctx.clearRect(0, 0, rect.width, rect.height);
+      ctx.fillStyle = "#222";
+      drawQwerty(ctx, rect.width);
+      ctx.fillStyle = "white";
       drawRecursive(ctx, 0, 0, rect.width, scope);
     }
   }, [scope]);
